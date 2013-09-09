0a8515aed3706e6b4fdaf3db1da85620b72a5aa7
jixiang58 <584795417@qq.com>
2013-09-09 08:36:47 +0800

    homework
    老师不好意思这周有补考科目所以提交作业比较迟....望见�

---
+++ Hello world/源1.cpp
@@ -0,0 +1,15 @@
+/*俸老师不好意思，这周末有补考科目，因此作业提交时间比较迟，望见谅，谢谢！
+
+c++第一次作业，编写helloworld，2013.9.8
+
+学生：姬祥
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	cout<<"Hello world!"<<endl;
+}
